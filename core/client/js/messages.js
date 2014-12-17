a3e8dab4f9c5a6087a858dbcd5ee677eafae66ea
Leo Li <kyli96@users.noreply.github.com>
2014-12-17 09:21:29 -0800

    updated css and js

---
+++ core/client/js/messages.js
@@ -0,0 +1,15 @@
+﻿var user;
+function showMessage(obj) {
+    $('#messages').append($('<li>').text(obj.name + ': ' + obj.msg));
+}
+var socket = io();
+$('form').submit(function () {
+    var msg = $('#message-input').val();
+    socket.emit('sendMessage', { username: user.username, name: user.name, msg: msg });
+    $('#message-input').val('');
+    return false;
+});
+socket.on('profile', function (obj) {
+    user = obj;
+})
+socket.on('receiveMessage', showMessage);
--- core/server/routes/frontendRoutes.js
+++ core/server/routes/frontendRoutes.js
@@ -5,6 +5,9 @@
 
 frontendRoutes = function (){
     var router = express.Router();
+    router.use('/js', express.static(path.join(__dirname, '/../../client/js')));
+    router.use('/css', express.static(path.join(__dirname, '/../../client/css')));
+
     router.get('/', frontendControllers.showSignin);
     router.use('/messages', express.static(path.join(__dirname, '/../../client/html/messages')));
     return router;
